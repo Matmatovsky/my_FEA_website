227bed25d116b700daab41c54b7a05cf5f6a6b3e
Mat Matovsky <matovskymat@gmail.com>
2022-08-17 19:49:00 +0200

    started to learn JS, nothing to see here

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let myFirstVar = 9;
+let mySecondVar = -2.1;
+console.log(myFirstVar*mySecondVar);
+console.log("--------------------------------------------")
+console.log(mySecondVar);
+mySecondVar += 3;
+console.log(mySecondVar);
+mySecondVar++;
+console.log(mySecondVar);
+console.log("--------------------------------------------")
+const pi = 3.14;
+console.log(pi)
+//pi++; can't update it, it stays constant
+let changinVar = 150;
+console.log(changinVar);
+changiVar = "lol";
+console.log(changinVar); //variables can change type! although apparently in TypeScript it can't
+console.log("--------------------------------------------")
